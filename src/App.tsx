66a67c51363f406a44e494da73df9b54400eda09
Matheus Santos <mateteus07042000@gmail.com>
2023-09-12 09:12:23 -0300

    ui-base

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { Github } from 'lucide-react'
 import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
 
 export function App() {
 
   return (
-    <Button>Teste</Button>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ia</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">NLW IA Trilha Mastery da Rocketseat</span>
+
+          <Separator orientation='vertical' className='h-6'/>
+
+          <Button variant="secondary">
+            <Github className="w-4 h-4 mr-2"/>
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
